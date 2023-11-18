becc2a1f045fb8597c76666105065bcb087cbecd
nekosenpai <121236723+NekoSenpai06@users.noreply.github.com>
2023-11-18 15:10:17 +0800

    Add files via upload

---
+++ pageScript.js
@@ -0,0 +1,11 @@
+function letter1() {
+     location.href = "letters/001.html";
+}
+
+function letter2() {
+     location.href = "letters/002.html";
+}
+
+function letter3() {
+     location.href = "letters/003.html";
+}
