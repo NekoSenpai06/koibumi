34763f6d65649d83ec2ba697b4d2955116f35ffd
nekosenpai <121236723+NekoSenpai06@users.noreply.github.com>
2023-12-06 19:30:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 var password = document.querySelector("input");
+var indicator = document.getElementById("indicator");
 
 function paswrd() {
      if(password.value === "us0607") {
+          indicator.innerHTML = "✔";
           location.href = "page.html";
      }
 
      else {
-          alert("X");
+          indicator.innerHTML = "✘";
      }
 }
